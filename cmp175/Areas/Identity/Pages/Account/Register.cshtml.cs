8442c00090f2e5756b11827ca10a74e9636a4ea4
Đặng Hồng Quân <99955870+DangHongQuan@users.noreply.github.com>
2024-04-05 18:26:57 +0700

    fix Register.cshtml

--- cmp175/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ cmp175/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -2,24 +2,28 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.WebUtilities;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
-using System.Text.Encodings.Web;
-using cmp175.Models;
-using Lab3_LTW_TH.Utilitys;
-namespace cmp175.Areas.Identity.Pages.Account {
+using Microsoft.Extensions.Logging;
 
-    public class RegisterModel : PageModel {
+namespace _2001_DANGHONGQUAN_webbansach.Areas.Identity.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
         private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly RoleManager<IdentityRole> _roleManager;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly IUserStore<IdentityUser> _userStore;
         private readonly IUserEmailStore<IdentityUser> _emailStore;
@@ -28,12 +32,11 @@ namespace cmp175.Areas.Identity.Pages.Account {
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
-            RoleManager<IdentityRole> roleManager,
             IUserStore<IdentityUser> userStore,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender) {
-            _roleManager= roleManager;
+            IEmailSender emailSender)
+        {
             _userManager = userManager;
             _userStore = userStore;
             _emailStore = GetEmailStore();
@@ -65,8 +68,8 @@ namespace cmp175.Areas.Identity.Pages.Account {
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public class InputModel {
-
+        public class InputModel
+        {
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -94,67 +97,30 @@ namespace cmp175.Areas.Identity.Pages.Account {
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
-
-            public string? Role { get; set; }
-            [ValidateNever]
-            public IEnumerable<SelectListItem> RoleList { get; set; }
-
-            [Required]
-
-            public string? StreetAddress { get; set; }
-            public string? City { get; set; }
-            public string? State { get; set; }
-            public string? PostalCode { get; set; }
-            public string? PhoneNumber { get; set; }
-            public string? Name { get; set; }
-
         }
 
-        public async Task OnGetAsync(string returnUrl = null) {
-            if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult()) {
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
 
-            }
-            Input = new() {
-                RoleList = _roleManager.Roles.Select(x => x.Name).Select(i => new SelectListItem {
-                    Text = i,
-                    Value = i
-
-                })
-            };
+        public async Task OnGetAsync(string returnUrl = null)
+        {
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
 
-        public async Task<IActionResult> OnPostAsync(string returnUrl = null) {
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
             returnUrl ??= Url.Content("~/");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-            if (ModelState.IsValid) {
+            if (ModelState.IsValid)
+            {
                 var user = CreateUser();
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
-                user.StreetAddress = Input.StreetAddress;
-                user.City = Input.City;
-                user.State = Input.State;
-                user.PostalCode = Input.PostalCode;
-                user.PhoneNumber = Input.PhoneNumber;
-                user.Name = Input.Name;
-
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
-                if (result.Succeeded) {
+                if (result.Succeeded)
+                {
                     _logger.LogInformation("User created a new account with password.");
-
-                    if (!String.IsNullOrEmpty(Input.Role)) {
-                        await _userManager.AddToRoleAsync(user, Input.Role);
-
-                    } else {
-                        await _userManager.AddToRoleAsync(user, SD.Role_Customer);
-                    }
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
@@ -168,14 +134,18 @@ namespace cmp175.Areas.Identity.Pages.Account {
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount) {
+                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    {
                         return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    } else {
+                    }
+                    else
+                    {
                         await _signInManager.SignInAsync(user, isPersistent: false);
                         return LocalRedirect(returnUrl);
                     }
                 }
-                foreach (var error in result.Errors) {
+                foreach (var error in result.Errors)
+                {
                     ModelState.AddModelError(string.Empty, error.Description);
                 }
             }
@@ -184,18 +154,24 @@ namespace cmp175.Areas.Identity.Pages.Account {
             return Page();
         }
 
-        private ApplicationUser CreateUser() {
-            try {
-                return Activator.CreateInstance<ApplicationUser>();
-            } catch {
+        private IdentityUser CreateUser()
+        {
+            try
+            {
+                return Activator.CreateInstance<IdentityUser>();
+            }
+            catch
+            {
                 throw new InvalidOperationException($"Can't create an instance of '{nameof(IdentityUser)}'. " +
                     $"Ensure that '{nameof(IdentityUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
                     $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
             }
         }
 
-        private IUserEmailStore<IdentityUser> GetEmailStore() {
-            if (!_userManager.SupportsUserEmail) {
+        private IUserEmailStore<IdentityUser> GetEmailStore()
+        {
+            if (!_userManager.SupportsUserEmail)
+            {
                 throw new NotSupportedException("The default UI requires a user store with email support.");
             }
             return (IUserEmailStore<IdentityUser>)_userStore;
